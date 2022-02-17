d12aa83aa3a940edda72ac52e9709b2fff612986
karlarob <karla@happyshuttlecancun.com>
2022-02-17 14:36:13 -0500

    Remove console.log

--- src/hooks/useMeals.tsx
+++ src/hooks/useMeals.tsx
@@ -1,27 +1,26 @@
-import { useEffect, useState } from 'react'
-import { Meal, Welcome } from '../interfaces/mealIntarface'
+import { useEffect, useState } from "react";
+import { Meal, Welcome } from "../interfaces/mealIntarface";
 
 export const useMeals = () => {
-  const [comida, setComida] = useState<Meal[]>([])
-  const [cargando, setCargando] = useState(true)
-  const [retry, setRetry] = useState(false)
+  const [comida, setComida] = useState<Meal[]>([]);
+  const [cargando, setCargando] = useState(true);
+  const [retry, setRetry] = useState(false);
 
   const name = async () => {
     const res = await fetch(
-      'https://www.themealdb.com/api/json/v1/1/random.php'
-    )
-    const data: Welcome = await res.json()
-    console.log('ll', data)
-    setComida(data.meals)
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const data: Welcome = await res.json();
+    setComida(data.meals);
     setTimeout(() => {
-      setCargando(false)
-    }, 5000)
-  }
+      setCargando(false);
+    }, 5000);
+  };
 
   useEffect(() => {
-    setCargando(true)
-    name()
-  }, [retry])
+    setCargando(true);
+    name();
+  }, [retry]);
 
-  return { comida, cargando, setRetry, retry }
-}
+  return { comida, cargando, setRetry, retry };
+};
